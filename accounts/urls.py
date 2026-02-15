ee9ffc06cc9f5ed5ce7d6071985b41050289ea16
Samira <samirakamaliaut@gmail.com>
2026-02-15 16:47:28 +0200

    urlmodification

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from .views import RegisterAPIView, MeAPIView, MyActivityLogsAPIView
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view()),
-    path("me/", MeAPIView.as_view()),
+    path("m/", MeAPIView.as_view()),
     path("logs/", MyActivityLogsAPIView.as_view()),
 ]
 
